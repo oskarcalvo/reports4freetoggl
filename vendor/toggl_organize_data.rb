a024bd3be0f12163e4cacc19cb48b130447b7784
Oskar Calvo <oscar.calvo@the-cocktail.com>
2015-10-21 00:08:12 +0200

    create a New Class: TogglOrganizeData.

---
+++ vendor/toggl_organize_data.rb
@@ -0,0 +1,17 @@
+class TogglOrganizeData
+
+  def initialize ( kind , data )
+      
+  end
+  
+  private
+  
+  def data_organize_by_tag (data)
+  
+  end
+  
+  def data_organize_by_name (data)
+  
+  end
+
+end
