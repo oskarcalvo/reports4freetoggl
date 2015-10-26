7d3c93979e1bed1071925bd9b530a7d6bcc16ee4
Oskar Calvo <oscar.calvo@the-cocktail.com>
2015-10-26 23:54:11 +0100

    Update the file toggl_organize_date: * Create the build_data method. * Update the method : data_organize_by_name.

--- vendor/toggl_organize_data.rb
+++ vendor/toggl_organize_data.rb
@@ -1,6 +1,14 @@
 class TogglOrganizeData
 
-  def initialize ( kind , data )
+  def build_data ( kind , data )
+  
+    if kind == 'description'
+      results = data_organize_by_name (data)
+      return results
+    elsif kind == 'tag'
+      results = data_organize_by_tag (data)
+      return results
+    end
       
   end
   
@@ -11,7 +19,25 @@ class TogglOrganizeData
   end
   
   def data_organize_by_name (data)
-  
+    tasks = Hash.new
+
+    data.each do | entry |
+    
+      description = entry['description']
+      duration = entry['duration']
+
+      if tasks[description].nil?
+        
+        tasks[description] = duration
+      
+      else
+
+        newduration = tasks[description] + duration
+        tasks[description] = newduration
+
+      end
+    end
+    return tasks
   end
 
 end
