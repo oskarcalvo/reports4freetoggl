812aad7173f7b0092991163c8841b7b0bbd8eb5b
vagrant <vagrant@debian>
2016-01-03 22:23:38 +0100

    Update the file to retrieve more information from users.

--- vendor/toggl_login.rb
+++ vendor/toggl_login.rb
@@ -2,7 +2,7 @@ class TogglLogin
 
   def get_toggl_user_data (user,pass)
 
-    uri = URI('https://www.toggl.com/api/v8/me')
+    uri = URI('https://www.toggl.com/api/v8/me?with_related_data=true')
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
     request = Net::HTTP::Get.new(uri.request_uri)
